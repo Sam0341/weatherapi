5436a7128948edc194bdfba53f7b5ed780641686
Sam0341 <144931877+Sam0341@users.noreply.github.com>
2024-11-18 10:35:19 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Lab 7 Project</h1>
+      <p>
+        Welcome! Go to the <Link href="/part-b">Part B</Link> page to see the WeatherAPI widget.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
